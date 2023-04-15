558fd2ed8497379d619b92329f4507dc13d1643c
domenicasantana <dasc1996@hotmail.com>
2023-04-15 12:10:49 +0000

    added function

--- run.py
+++ run.py
@@ -14,7 +14,17 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60")
 
-print(data)
+    data_str = input("Enter your data here: ")
+    print(f"the data provided is {data_str}")
+
+
+get_sales_data()
 
